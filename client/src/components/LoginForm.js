29a6cba587106159ee169d1a27ee322e88ff039a
palminski <williambolls98@gmail.com>
2022-11-29 14:47:48 -0800

    completed, just needs some minor touchups

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -34,7 +34,7 @@ const LoginForm = () => {
       const {data} = await loginUser({
         variables: {...userFormData}
       });
-      
+      console.log(data);
       Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
