a5e7889b879382e76a7c0042d5d7fa8161ff1147
palminski <williambolls98@gmail.com>
2022-11-30 08:53:40 -0800

    removed old api routes

--- server/server.js
+++ server/server.js
@@ -43,13 +43,3 @@ const startApolloServer = async (typeDefs,resolvers) => {
 };
 startApolloServer(typeDefs,resolvers);
 
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
