55846722037c720e8d2c4d2c9093fb52f5336659
palminski <williambolls98@gmail.com>
2022-11-30 09:42:49 -0800

    use got autocorrected to unsubscribe or something, fixed it

--- server/server.js
+++ server/server.js
@@ -27,10 +27,10 @@ const startApolloServer = async (typeDefs,resolvers) => {
   //integrate server with express app as middleware
   server.applyMiddleware({app});
   if (process.env.NODE_ENV === 'production') {
-    app.unsubscribe(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
   }
 
-  app.get('/',(req,res) => {
+  app.get('*',(req,res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
